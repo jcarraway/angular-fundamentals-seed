6c772e7f1034e5e089ff1ea388b96af4a441faeb
Jarrett Carraway <jarrett.s.carraway@gmail.com>
2018-02-26 15:18:36 -0500

    halfway

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
+
 @NgModule({
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    RouterModule.forRoot(routes),
+    PassengerDashboardModule
   ],
-  bootstrap: [
-    AppComponent
-  ],
-  declarations: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
